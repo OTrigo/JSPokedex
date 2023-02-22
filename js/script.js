e6d3a85823b0419baa718f5e235d926723f24dc2
Jorge Lucas <37757146+thepinkwheat@users.noreply.github.com>
2023-02-22 01:56:33 -0300

    Pesquisa e navegação da API
    Adicionando funcionalidade aos botões de next e prev e adicionando pesquisa por nome e ID

--- js/script.js
+++ js/script.js
@@ -1,28 +1,57 @@
 const pokemonName = document.querySelector('.pokemon__name')
-
 const pokemonNumber = document.querySelector('.pokemon__number')
-
 const pokeImage = document.querySelector('.pokeImage')
+
+const form = document.querySelector('form')
+const input = document.querySelector('.input__search')
 
 const prev = document.querySelector('.btn-prev')
 const next = document.querySelector('.btn-next')
 
-let index = 1
+let indexPokemon = 1
 
 const fetchPokemon = async (pokemon)=>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    if(APIResponse.status === 200){
+        const data = await APIResponse.json();
+        return data;   
+    }
 }
 
 const renderPokemon = async (pokemon)=>{
-
+    pokemonName.innerHTML = 'Carregando...';
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    if(data){
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
+        input.value = ''
+    }
+    else{
+        pokemonName.innerHTML = 'Not Found'
+        pokemonNumber.innerHTML = 'Missigno'
+        pokeImage.src = '#'
+    }
 }
 
-renderPokemon(24);
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase())
+})
+
+next.addEventListener("click", ()=>{
+    indexPokemon++
+    renderPokemon(indexPokemon)
+})
+
+prev.addEventListener("click", ()=>{
+    if(indexPokemon>1){
+        indexPokemon--
+        renderPokemon(indexPokemon)
+    }
+})
+
+renderPokemon(indexPokemon)
+
